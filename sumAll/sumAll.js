3632ad30cc9152959a1af5c9084ebdf479253b7c
Binyomin Freedman <binyomin.freedman@digitalbridge.com>
2020-12-16 14:47:23 +0000

    passed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let total = 0;
+    
+    if (isNaN(start) || isNaN(end)) {
+        return "ERROR";
+    }
+    
+    if (typeof start === 'string' || typeof end === 'string') {
+        return "ERROR";
+    }
 
+
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    if (start > end) {
+        [start, end] = [end, start]; // swap the variables
+    }
+
+    for (let i = start; i < end + 1; i++) {
+        total += i; 
+    }
+    return total;
 }
 
 module.exports = sumAll
