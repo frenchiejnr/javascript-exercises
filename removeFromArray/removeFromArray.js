2aeb3fbb4e00bb09fb51af9473283ff8582bdeaf
Binyomin Freedman <binyomin.freedman@digitalbridge.com>
2020-12-16 14:23:35 +0000

    passed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++) {
+        let toRemoveIndex = array.indexOf(arguments[i]);
+        if (toRemoveIndex === -1) {
+            continue;
+        }
+        array.splice(toRemoveIndex,1);
+    }
+    return array;
 }
 
 module.exports = removeFromArray
