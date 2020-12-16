b6fe68f374be23a2d95cc4e0b010d9e5d69b1dbc
Binyomin Freedman <binyomin.freedman@digitalbridge.com>
2020-12-16 12:52:46 +0000

    passed repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    let repeatedString = '';
+    if (repeats < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < repeats; i++) {
+        repeatedString += string;  
+    }
+    return repeatedString;
 }
 
 module.exports = repeatString
