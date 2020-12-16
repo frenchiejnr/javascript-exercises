4d37e9d03b610b7fa4bbceaa849ffdc54cf69f32
Binyomin Freedman <binyomin.freedman@digitalbridge.com>
2020-12-16 15:37:21 +0000

    passed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,24 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celcius = ((farenheit - 32) *(5/9));
+  celcius = roundTo1Decimal(celcius);
+  return celcius;
 }
 
-const ctof = function() {
+const ctof = function(celcius) {
+  let farenheit = (celcius * (9/5))+ 32;
+  farenheit = roundTo1Decimal(farenheit);
+  return farenheit;
+}
 
+function roundTo1Decimal(num) {
+  if (num % 1 != 0) {
+    num = parseFloat(num.toFixed(1));
+  }
+  return num;
 }
 
 module.exports = {
   ftoc,
   ctof
 }
+
