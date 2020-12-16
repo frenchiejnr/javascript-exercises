dabf175540ef593f4ff1c66518297195ad71661c
Binyomin Freedman <binyomin.freedman@digitalbridge.com>
2020-12-16 13:35:35 +0000

    passed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversedString = [];
+    for (let i = splitString.length -1; i >= 0; i--) {
+        reversedString.push(splitString[i]);
+    }
+    return reversedString.join("");
 }
+
+reverseString("hello");
 
 module.exports = reverseString
