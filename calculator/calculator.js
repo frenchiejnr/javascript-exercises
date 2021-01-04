3abcba5ed8a0bfa73d6b934808020a9fb1d9d770
frenchiejnr <frenchiejnr@gmail.com>
2021-01-04 12:59:52 +0000

    Solved Calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (numbers) {
+	const summed = numbers.reduce((acc, number) => {
+		return acc + number;
+	} ,0)
+
+	return summed;
 }
 
-function multiply () {
-	
+function multiply (numbers) {
+	const multiplied =  numbers.reduce((acc, number) => {
+		return acc * number;
+	})
+
+	return multiplied;
 }
 
-function power() {
-	
+function power(a,b) {
+	return Math.pow(a,b);
 }
 
-function factorial() {
-	
+function factorial(number) {
+	if (number === 0 || number === 1) {
+		return 1;
+	}
+	let total = number;
+	while (number > 1) {
+		number--;
+		total = total * number;
+	}
+	return total;
 }
 
 module.exports = {
