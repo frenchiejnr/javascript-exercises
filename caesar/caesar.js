ca84dc179e6ce6832b2ed2345c3ad2aaa1dde4cb
frenchiejnr <frenchiejnr@gmail.com>
2021-01-05 11:36:15 +0000

    Solve caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,27 @@
-const caesar = function() {
+const caesar = function (string, shift) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let caesarResult = "";
 
-}
+    // If shift is negative, convert to positive
+    if (shift < 0) {
+        shift = alphabet.length + shift;
+    }
 
-module.exports = caesar
+    [...string].forEach((letter) => {
+        console.log(letter);
+        let letterIndex = alphabet.indexOf(letter.toLowerCase()) % alphabet.length;
+        let shiftedIndex = (letterIndex + shift) % alphabet.length;
+        if (/[a-zA-Z]/.test(letter)) {
+            if (letter === letter.toUpperCase()) {
+                caesarResult += alphabet[shiftedIndex].toUpperCase();
+            } else {
+                caesarResult += alphabet[shiftedIndex];
+            }
+        } else {
+            caesarResult += letter;
+        }
+    });
+    return caesarResult;
+};
+
+module.exports = caesar;
