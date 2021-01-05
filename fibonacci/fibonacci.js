12bcd54a644db0113c22731a8bb7681c779bd986
frenchiejnr <frenchiejnr@gmail.com>
2021-01-05 14:15:55 +0000

    Passed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fibonacciArray = [1, 1]
+    let i = num
+    do {
+        console.log(i);
+        console.log(fibonacciArray[fibonacciArray.length - 1]);
+        fibonacciArray.push(fibonacciArray[fibonacciArray.length - 1] + fibonacciArray[fibonacciArray.length - 2])
+        i--;
+    } while (i > 2);
+    return fibonacciArray[num - 1];
 }
-
 module.exports = fibonacci
