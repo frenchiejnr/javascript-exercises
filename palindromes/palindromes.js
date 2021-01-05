7ded8a9426bd2b7383fe71fb8c6cb7a8b1f84a30
frenchiejnr <frenchiejnr@gmail.com>
2021-01-05 13:53:59 +0000

    Passed palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function() {
+const palindromes = function (potentialPalindrome) {
+    let palindrome = true;
 
+    // remove punctuation, including whitespace
+    let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g;
+    potentialPalindrome = potentialPalindrome.replace(regex, "");
+
+    potentialPalindrome = potentialPalindrome.toLowerCase();
+
+    let length = potentialPalindrome.length - 1;
+    potentialPalindrome = [...potentialPalindrome];
+    for (let i = 0; i < potentialPalindrome.length; i++) {
+        if (potentialPalindrome[i] !== potentialPalindrome[length - i]) {
+            palindrome = false;
+            break;
+        }
+    }
+    return palindrome;
 }
+
+palindromes('A car, a man, a maraca.');
 
 module.exports = palindromes
