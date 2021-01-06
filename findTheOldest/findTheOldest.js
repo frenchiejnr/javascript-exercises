5f23582e4986b48d55993d325c3d05485ddf7b32
frenchiejnr <frenchiejnr@gmail.com>
2021-01-06 13:12:17 +0000

    Passed findTheOldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,36 @@
-let findTheOldest = function() {
+let findTheOldest = function(peopleArray) {
+peopleArray.sort((a,b)=> {
+        if (!("yearOfDeath" in a)) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (!("yearOfDeath" in b)) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        aAge = a.yearOfDeath - a.yearOfBirth;
+        bAge = b.yearOfDeath - b.yearOfBirth;
+        return aAge < bAge ? 1 : -1;
+    })
 
+return peopleArray[0];
 }
+
+const people = [
+    {
+      name: 'Carly',
+      yearOfBirth: 1066,
+    },
+    {
+      name: 'Ray',
+      yearOfBirth: 1962,
+      yearOfDeath: 2011
+    },
+    {
+      name: 'Jane',
+      yearOfBirth: 1912,
+      yearOfDeath: 1941
+    },
+  ]
+
+  findTheOldest(people);
 
 module.exports = findTheOldest
